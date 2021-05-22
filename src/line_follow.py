7ea681514b18d7a6e5cec2542a24f4915cabaffd
tasashi0805 <66871676+tasashi0805@users.noreply.github.com>
2021-05-23 01:33:25 +0930

    Add files via upload

---
+++ src/line_follow.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python 
+
+import rospy, cv2, cv_bridge
+import numpy as np
+from std_msgs.msg import Float64
+from sensor_msgs.msg import Image, CameraInfo
+from geometry_msgs.msg import Twist
+
+
+class Drive:
+	def __init__(self):
+		self.bridge = cv_bridge.CvBridge()
+		self.drive_pub= rospy.Publisher("drive",Float64,queue_size=10)
+		self.image_sub = rospy.Subscriber('/camera/rgb/image_raw', Image, self.image_callback)
+		self.cmd_vel_pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
+		self.twist = Twist()
+		self.cnt = 0
+
+	def image_callback(self, msg):
+		self.cnt += 1
+		image = self.bridge.imgmsg_to_cv2(msg,desired_encoding='bgr8')
+		hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+#detect color of the map that wanted  robot to follow
+
+		red_lower_range = np.array([0,50,50])
+		red_upper_range = np.array([10,255,255])
+		mask = cv2.inRange(hsv, red_lower_range, red_upper_range)
+		#cv2.imshow("img:", mask)
+		#cv2.waitKey(0)
+#shape of image 
+		h, w, d = image.shape
+		search_top = 3*h/4
+		search_bot = 3*h/4 + 20 
+
+		mask[0:int(search_top), 0:w] = 0
+		mask[int(search_bot):h, 0:w] = 0
+#get the center points of blob 
+		M = cv2.moments(mask)
+		if M['m00'] > 0:
+			cx = int(M['m10']/M['m00'])
+			cy = int(M['m01']/M['m00'])
+#error numbers in the real environment when the robot moving 
+			err = cx - w/2
+
+			#self.twist.angular.z = -float(err) /100
+			self.drive_pub.publish(err)
+
+		##cv2.imshow("mask",mask)
+		##cv2.imshow("output", image)
+		##print("output Image")
+		##cv2.waitKey(3)#display the window until any keypress 
+		#cv2.waitkey(1) #display a frame for 1ms, after that 
+if __name__ == '__main__':
+	print('start')
+	rospy.init_node('drive')
+	drive = Drive()
+	rospy.spin()
